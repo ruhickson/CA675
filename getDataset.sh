983003f0494617b0666c4e6a2aa485e6e0a56ce1
Petras Butkus <petras.butkus2@mail.dcu.ie>
2018-04-05 11:28:55 +0100

    Add new file

---
+++ getDataset.sh
@@ -0,0 +1,4 @@
+mkdir /home/cloudera/Desktop/CA675Assignment2 &&
+cd /home/cloudera/Desktop/CA675Assignment2/ &&
+wget https://www.kaggle.com/safecast/safecast/downloads/safecast.zip/2 &&
+unzip safecast.zip
