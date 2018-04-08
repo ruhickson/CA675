89e33599539f239cf32fd54097e0edf5db791631
Petras Butkus <petras.butkus2@mail.dcu.ie>
2018-04-08 12:59:01 +0100

    moved tomain hivetable.sh

--- hivetable.sh
+++
@@ -1,2 +0,0 @@
-hive -e "CREATE DATABASE ca675db;" &&
-hive -e "CREATE TABLE ca675db.measurements (capturetime STRING, latitude STRING, longitude  STRING, value STRING, unit  STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED AS TEXTFILE;"
