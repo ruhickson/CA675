ae807a25e3bb00dea2fd018ade7093b2b2e18fda
Petras Butkus <petras.butkus2@mail.dcu.ie>
2018-04-08 12:35:23 +0100

    Update getDataset.sh now it will create measurements-out.csv (around 10GB) and testmeasurements-out.csv            (999 records, around 120 KB)

--- getDataset.sh
+++ getDataset.sh
@@ -2,4 +2,12 @@ mkdir /home/cloudera/Desktop/CA675Assignment2 &&
 cd /home/cloudera/Desktop/CA675Assignment2/ &&
 wget https://api.safecast.org/system/measurements.tar.gz &&
 tar -xvzf measurements.tar.gz &&
-rm measurements.tar.gz
+rm measurements.tar.gz &&
+head -1000 measurements-out.csv > testmeasurements-out.csv &&
+echo "################################################################
+#							                                   #
+# measurements-out.csv and testmeasurements-out.csv is created #
+#							                                   #
+################################################################"
+
+
