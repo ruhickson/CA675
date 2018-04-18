f5c89a9571e590514b2476aa49360563749acd3a
Petras Butkus <petras.butkus2@mail.dcu.ie>
2018-04-18 18:13:57 +0100

    Update testCreateHiveDBandTable.sh

--- test/testCreateHiveDBandTable.sh
+++ test/testCreateHiveDBandTable.sh
@@ -1,2 +1,2 @@
 hive -e "CREATE DATABASE testca675db;" &&
-hive -e "CREATE TABLE testca675db.measurements (capturetime STRING, latitude STRING, longitude  STRING, value STRING, unit  STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED AS TEXTFILE;"
+hive -e "CREATE TABLE testca675db.measurements (capturetime STRING, latitude STRING, longitude  STRING, value INT, unit  STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED AS TEXTFILE;"
